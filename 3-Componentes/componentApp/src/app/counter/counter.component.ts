3d29f00dadd368d25336c59e3d083b609596dc56
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-01-23 18:48:24 -0300

    Ahora el componente counter guarda el numero en local por si se cierra el navegador

--- 3-Componentes/componentApp/src/app/counter/counter.component.ts
+++ 3-Componentes/componentApp/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -7,14 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css'
 })
-export class CounterComponent {
+// Definición de la clase CounterComponent que implementa la interfaz OnInit
+export class CounterComponent implements OnInit {
 
-  //Variable contador inicializado.
+  // Declaración e inicialización de la variable 'counter' con un valor inicial de 0
   counter: number = 0;
 
-  //Incremento del contador
+  // Método requerido por la interfaz OnInit, que se ejecuta una vez que el componente es inicializado
+  ngOnInit(): void {
+    // Recupera el valor almacenado en localStorage con la clave 'counter'
+    // Si no existe, inicializa el contador en 0 usando el operador lógico "||"
+    this.counter = parseInt(localStorage.getItem('counter')!) || 0;
+
+    // Imprime un mensaje en la consola para indicar que el componente fue creado
+    console.log("Creando componente");
+  }
+
+  // Método para incrementar el valor del contador
   setCounter(): void {
+    // Incrementa el valor actual de la variable 'counter' en 1
     this.counter++;
 
+    // Actualiza el valor del contador en localStorage con el nuevo valor convertido a string
+    localStorage.setItem('counter', this.counter.toString());
   }
 }
