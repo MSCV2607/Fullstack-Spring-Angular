3942cf47b387bc79b179c6516c56aba98d0b5e85
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-01-23 19:25:36 -0300

    Transmite datos hijos apadre

--- 3-Componentes/componentApp/src/app/app.component.ts
+++ 3-Componentes/componentApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
 
@@ -10,9 +10,25 @@ import { CounterComponent } from './counter/counter.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // Inicialización del contador con un valor predeterminado de 0
+  counter: number = 0;
+
+  // Título principal de la aplicación
   title = 'componentApp';
 
   // Subtítulo que será enviado al componente CounterComponent como una propiedad de entrada
   subTitle = 'Contador con estado de sesion';
+
+  // Método requerido por la interfaz OnInit, ejecutado una vez que el componente es inicializado
+  ngOnInit(): void {
+    // Recupera el valor almacenado en localStorage con la clave 'counter'
+    // Si no existe, inicializa el contador en 0 usando el operador lógico "||"
+    this.counter = parseInt(localStorage.getItem('counter')!) || 0;
+  }
+
+  // Método que actualiza el valor del contador cuando recibe un evento del componente hijo
+  setCounter(event: number): void {
+    this.counter = event;
+  }
 }
--- 3-Componentes/componentApp/src/app/counter/counter.component.ts
+++ 3-Componentes/componentApp/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -13,8 +13,11 @@ export class CounterComponent implements OnInit {
   // Declaración e inicialización de la variable 'counter' con un valor inicial de 0
   counter: number = 0;
 
-  // Declaración de la propiedad de entrada 'title', que recibirá un valor desde el componente padre.
+  // Declaración de la propiedad de entrada 'title', que recibirá un valor desde el componente padre
   @Input() title!: string;
+
+  // Evento que se emitirá al componente padre con el nuevo valor del contador
+  @Output() counterEmmit: EventEmitter<number> = new EventEmitter();
 
   // Método requerido por la interfaz OnInit, que se ejecuta una vez que el componente es inicializado
   ngOnInit(): void {
@@ -33,5 +36,8 @@ export class CounterComponent implements OnInit {
 
     // Actualiza el valor del contador en localStorage con el nuevo valor convertido a string
     localStorage.setItem('counter', this.counter.toString());
+
+    // Emite el nuevo valor del contador al componente padre
+    this.counterEmmit.emit(this.counter);
   }
 }
