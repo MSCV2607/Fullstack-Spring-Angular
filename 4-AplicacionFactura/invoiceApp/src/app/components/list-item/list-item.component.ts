c1c4ed5def1bd65d469e226ec34dfcd21ccfa968
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-01-24 20:55:40 -0300

    Creacion de componentes hijos

---
+++ 4-AplicacionFactura/invoiceApp/src/app/components/list-item/list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-item.component.html',
+
+})
+export class ListItemComponent {
+
+}
