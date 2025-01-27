4f2690bce100f677a204b343ab0a126040a14487
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-01-27 18:25:11 -0300

    Modelos carrito

---
+++ 5-CartApp/cartApp/src/app/data/product.data.ts
@@ -0,0 +1,35 @@
+import { Product } from "../models/product";
+
+export const products:Product[] = [
+    {
+    id: 1,
+    name: 'Phone XL',
+    description: 'A large phone with one of the best screens',
+    price: 799,
+    },
+    {
+    id: 2,
+    name: 'Phone Mini',
+    description: 'A great phone with one of the best cameras',
+    price: 699,
+    },
+    {
+    id: 3,
+    name: 'Phone Standard',
+    description: '',
+    price: 299,
+    },
+    {
+    id: 4,
+    name: 'Phone Standard',
+    description: '',
+    price: 299,
+    },
+    {
+    id: 5,
+    name: 'Phone Standard',
+    description: '',
+    price: 299,
+    },
+    
+]
