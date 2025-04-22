1b47b490ce35f63d71f5dfc70a5c09ff820b06f2
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-04-22 16:26:16 -0300

    Creando el componente Form

--- 7-user-app/src/app/components/user-app.component.ts
+++ 7-user-app/src/app/components/user-app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from '../models/user';
 import { UserComponent } from './user/user.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @Component({
   selector: 'user-app',
   standalone: true,
-  imports: [UserComponent],
+  imports: [UserComponent, UserFormComponent],
   templateUrl: './user-app.component.html',
 
 })
@@ -22,6 +23,8 @@ export class UserAppComponent {
     this.service.findAll().subscribe(users => this.users = users);
   }
 
-
+  addUser(user: User) {
+    this.users = [... this.users, {...user}];
+  }
 
 }
