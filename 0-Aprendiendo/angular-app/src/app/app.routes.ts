2c0a374988fbbce16ad19cdb564f0784052cd4d5
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-04-23 11:57:13 -0300

    Reacomodo

--- 0-Aprendiendo/angular-app/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  // Aquí se definen las rutas de la aplicación
-  // Ejemplo:
-  // { path: 'home', component: HomeComponent }
-];
