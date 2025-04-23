48b6f57a9a622d1798b1e6f13e5bf5d5ccc09d18
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-04-23 12:01:34 -0300

    Creando proyecto Backend con Spring Boot

---
+++ Spring/usersBackend/src/test/java/com/example/usersBackend/UsersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
