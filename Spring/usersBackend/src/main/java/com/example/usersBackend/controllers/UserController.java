886de5cfda440e241a7401a85e130ef5e39cbbb3
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-05-14 09:21:48 -0300

    Implementando el controlador RestController metodos handler GET

---
+++ Spring/usersBackend/src/main/java/com/example/usersBackend/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.example.usersBackend.controllers;
+
+
+import com.example.usersBackend.entities.User;
+import com.example.usersBackend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<User> list() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> show(@PathVariable Long id) {
+        Optional<User> userOptional = service.findById(id);
+        if (userOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(userOptional.orElseThrow());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Collections.singletonMap("error", "el usuario no se encontro por el id:" + id));
+    }
+
+}
