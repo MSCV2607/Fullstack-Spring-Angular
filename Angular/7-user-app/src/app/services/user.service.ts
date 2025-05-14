b4d4e50b7c83cd734c7df8560cbda05f2afbdf0f
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-05-14 16:42:50 -0300

    Consumindo desde Angular el API RESTful GET findById del Backend

--- Angular/7-user-app/src/app/services/user.service.ts
+++ Angular/7-user-app/src/app/services/user.service.ts
@@ -10,12 +10,17 @@ export class UserService {
 
   //Datos de ejemplo
   private users: User[] = [];
+  private url = 'http://localhost:8080/api/users';
 
   constructor(private http: HttpClient) { }
 
   //Obtener los usuarios
   //El método findAll() devuelve un Observable de tipo User[]
-  findAll(): Observable<User[]>{
-    return this.http.get<User[]>('http://localhost:8080/api/users');
+  findAll(): Observable<User[]>{8080
+    return this.http.get<User[]>(this.url);
+  }
+
+  findById(id: number) : Observable<User>{
+    return this.http.get<User>(this.url + '/' + id);
   }
 }
